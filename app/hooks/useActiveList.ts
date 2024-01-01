45dce46c7fe0dfc7d5b5fb69374202b8aea6487a
mercenarioz <funnystar320@gmail.com>
2024-01-01 18:45:13 +0700

    Add Pusher channel authorization endpoint and transport Update dependencies: zustand

---
+++ app/hooks/useActiveList.ts
@@ -0,0 +1,21 @@
+import { create } from "zustand";
+
+interface ActiveListStoreProps {
+  members: string[];
+  add: (id: string) => void;
+  remove: (id: string) => void;
+  set: (ids: string[]) => void;
+}
+
+const useActiveList = create<ActiveListStoreProps>((set) => ({
+  members: [],
+  
+  add: (id) => set((state) => ({ members: [...state.members, id] })),
+
+  remove: (id) =>
+    set((state) => ({
+      members: state.members.filter((memId) => memId !== id),
+    })),
+
+  set: (ids) => set(() => ({ members: ids })),
+}));
